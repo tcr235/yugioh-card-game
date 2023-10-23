3505fab012bcec08906a5fddccb0357edf04a0d6
Thiago Conte <tcr2@cin.ufpe.br>
2023-10-23 12:22:47 -0300

    feat: added card mechanics

--- src/scripts/engine.js
+++ src/scripts/engine.js
@@ -16,7 +16,37 @@ const state = {
     actions: {
         button: document.getElementById('next-duel')
     },
-}
+};
+
+const pathImages = ".src/assets/icons/";
+
+const cardData = [
+    {
+        id: 0,
+        name: "Blue Eyes White Dragon",
+        type: "Paper",
+        img: `${pathImages}dragon.png`,
+        WinOf: [1],
+        LoseOf: [2],
+    },
+    {
+        id: 1,
+        name: "Dark Magician",
+        type: "Rock",
+        img: `${pathImages}magician.png`,
+        WinOf: [2],
+        LoseOf: [0],
+    },
+    {
+        id: 2,
+        name: "Exodia",
+        type: "Scissors",
+        img: `${pathImages}exodia.png`,
+        WinOf: [0],
+        LoseOf: [1],
+    },
+    
+];
 
 function init() {
 
