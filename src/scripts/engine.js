1d6db7c68d09b5d8960e988430abae1fd5da6eba
Thiago Conte <tcr2@cin.ufpe.br>
2023-10-23 12:58:33 -0300

    feat: field card logic implemented

--- src/scripts/engine.js
+++ src/scripts/engine.js
@@ -18,7 +18,12 @@ const state = {
     },
 };
 
-const pathImages = ".src/assets/icons/";
+const playerSides = {
+    player1: "player-cards",
+    computer: "computer-cards",
+};
+
+const pathImages = "./src/assets/icons/";
 
 const cardData = [
     {
@@ -48,8 +53,44 @@ const cardData = [
     
 ];
 
-function init() {
+async function getRandomCardId() {
+    const randomIndex = Math.floor(Math.random() * cardData.length);
+    return cardData[randomIndex].id;
+};
 
+async function createCardImage(cardId, fieldSide) {
+    const cardImage = document.createElement("img");
+    cardImage.setAttribute("height", "100px");
+    cardImage.setAttribute("src", "./src/assets/icons/card-back.png");
+    cardImage.setAttribute("data-id", cardId);
+    cardImage.classList.add("card");
+
+    if(fieldSide === playerSides.player1){
+        cardImage.addEventListener("click", () => {
+            setCardsField(cardImage.getAttribute("data-id"));
+        });
+    }
+
+    cardImage.addEventListener("mouseover", () => {
+        drawSelectCard(cardId);
+    });
+
+    return cardImage;
+
+};
+
+async function drawCards(cardsNumber, fieldSide) {
+    for(let i = 0; i < cardsNumber; i++) {
+        const randomIdCard = await getRandomCardId();
+        const cardImage = await createCardImage(randomIdCard, fieldSide);
+
+        document.getElementById(fieldSide).appendChild(cardImage);
+    }
+};
+
+function init() {
+    drawCards(5, playerSides.player1);
+    drawCards(5, playerSides.computer);
 }
 
 init();
