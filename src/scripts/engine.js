224348db5e1b911ce17d5ef56026f4134cbecee2
Thiago Conte <tcr2@cin.ufpe.br>
2023-10-24 16:16:54 -0300

    refactor: setCardsField method

--- src/scripts/engine.js
+++ src/scripts/engine.js
@@ -90,16 +90,37 @@ async function setCardsField(cardId) {
 
     let computerCardId = await getRandomCardId();
 
-    state.fieldCards.player.style.display = "block";
-    state.fieldCards.computer.style.display = "block";
+    await showHiddenCardFields(true);
 
-    state.fieldCards.player.src = cardData[cardId].img;
-    state.fieldCards.computer.src = cardData[computerCardId].img;
+    await hiddenCardDetails();
+
+    await drawCardsInField(cardId, computerCardId);
 
     let duelResults = await checkDuelResults(cardId, computerCardId);
 
     await updateScore();
     await drawButton(duelResults);
+};
+
+async function drawCardsInField(cardId, computerCardId) {
+    state.fieldCards.player.src = cardData[cardId].img;
+    state.fieldCards.computer.src = cardData[computerCardId].img;
+};
+
+async function showHiddenCardFields(value) {
+    if(value === true){
+        state.fieldCards.player.style.display = "block";
+        state.fieldCards.computer.style.display = "block";
+    } else {
+        state.fieldCards.player.style.display = "none";
+        state.fieldCards.computer.style.display = "none";
+    }
+};
+
+async function hiddenCardDetails() {
+    state.cardSprites.avatar.src = "";
+    state.cardSprites.name.innerText = "";
+    state.cardSprites.type.innerText = "";
 };
 
 async function updateScore() {
@@ -171,6 +192,8 @@ async function playAudio(status) {
 }
 
 function init() {
+    showHiddenCardFields(false);
+
     drawCards(5, playerSides.player1);
     drawCards(5, playerSides.computer);
 }
